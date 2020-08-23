4565066dd61e68a9e97d5815aa11ede4bf1feae6
techsoft705 <techsoft705@gmail.com>
2020-08-23 19:01:01 +0700

    update files

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ["**/lib/**"],
   projects: [{
-     dispayName: 'e2e test',
+     dispayName: 'e2e testing',
      testMatch: ['<rootDir>/__tests__/**/*.test.js']
   }]
 }
