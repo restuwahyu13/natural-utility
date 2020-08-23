28d8627b370d0b124e9ea5957c205c61467988d8
techsoft705 <techsoft705@gmail.com>
2020-08-23 18:49:36 +0700

    jest config updated

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,8 @@
 module.exports = {
-  verbose: true,
   collectCoverage: true,
-  coverageDirectory: "./coverage/",
   collectCoverageFrom: ["**/lib/**"],
-  roots: ["__tests__/"],
-  testMatch: [
-    "__tests__/.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)",
-     "lib/.[jt]s?(x)", "**/?(*.)+(js|ts).[jt]s?(x)"
-    ],
-  coveragePathIgnorePatterns: ["node_modules", "small.dumy.json","big.dumy.json"],
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-  notify: true,
-  testTimeout : 80000
+  projects: [{
+     dispayName: 'e2e test',
+     testMatch: ['<rootDir>/__tests__/**/*.test.js']
+  }]
 }
